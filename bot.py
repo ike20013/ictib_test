f1f8d6b40eb29b817614344d5632ba384f2537e6
krinjmann <tavklandre@gmail.com>
2020-09-24 03:00:04 +0300

    Fix bug

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import telebot
-from flask inport Flask, request
+from flask import Flask, request
 
 TOKEN = '1271897314:AAG1c6rZ8JnGtRA2Mx1mJ1A7LOHCo_-ysVY'
 bot = telebot.TeleBot(token=TOKEN)
