0d474646ef73d4a4df5068c0ea5054ef4beac434
krinjmann <tavklandre@gmail.com>
2020-10-19 02:27:06 +0300

    fix webhooks

--- bot.py
+++ bot.py
@@ -332,7 +332,7 @@ def getMessage():
 @server.route("/")
 def webhook():
    bot.remove_webhook()
-   bot.set_webhook(url='https://infinite-waters-23955.herokuapp.com/' + TOKEN)
+   bot.set_webhook(url='https://ictibochka.herokuapp.com/' + TOKEN)
    return "!", 200
 if __name__ == "__main__":
    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
